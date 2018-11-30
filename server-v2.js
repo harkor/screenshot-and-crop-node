7dfdede31b7fc41dfede90ededb5fac6d0e29c4c
Julien W <julien@harkor.be>
2018-11-30 16:51:38 +0100

    .

--- server-v2.js
+++
@@ -1,9 +0,0 @@
-/* Ouvre le navigateur, je ne pense pas que ça marchera sur un serveur sans interface graphique */
-
-var app = require("node-server-screenshot");
-
-app.fromURL("http://todomvc.com", "test.png", function(){
-
-    console.log('done');
-
-});
